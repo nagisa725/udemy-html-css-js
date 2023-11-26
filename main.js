05e285595b4c0a1bb45dcbfe647e6689c6712aba
hamadanagisa <kaeno.ana@gmail.com>
2023-11-27 04:57:50 +0900

    メソッドとプロパティについて

--- main.js
+++ main.js
@@ -1,56 +1,9 @@
-
-// let name1 = "hamaie ryuuiti";
-// console.log("hello" + name1 );
-// let name2 = "hamaie ryuuiti2";
-// console.log("hello" + name2)
-// 上記の記述は記述量やバグなどが起こりやすいためあまり良くない
-
-// その為関数を使う
-function hello(name) {
-    console.log("hello"+name);
-}
-// ()内の引数は複数入れるの可能、(name,age) ets...
-hello("hamaie ryuuiti3");
-hello("hamaie ryuuiti4");
-
-function hello(name,age) {
-    console.log("hello"+name+age);
-    return name + age ;
-    // 戻り値
-}
-hello("code mafia", 20);
-const returnval = hello("code mafia",30)
-console.log(returnval);
-
-
-
-function characters(name,age) {
-    console.log("kawaii"+name+age);
-    return name+age;
-}
-characters("ちいかわ",1);
-characters("おぱんちゅうさぎ")
-
-const returnval2 = characters("ちいかわ",1);
-console.log(returnval2);
-
-// 同様な意味を持った関数の定義の仕方
-const hello2 = function(name,age){
-    console.log("hello"+name+age);
-    return name+age;
-}
-hello("code mafia",20);
-// 変数に関数を代入している
-
-const character = function(name,age){
-    console.log("omosiroi"+name+age);
-    return name+age;
-}
-character("ちいかわ",2);
-
-// 無名関数　アノニマスファンクション
-function(name,age){
-    console.log("hello"+name+age);
-    return name+age;
-}
-hello("code mafia",20);
+let hello = "heloo world";
+console.log(hello.length);
+// length 文字列の長さを取得できる
+// toUpperCase() 文字列が大文字表記になる
+// substring(0,?) 与えられた文字列分だけ表記する
+// slice 同上
+// split() 文字を分割する,空文字だと一文字ずつ表記される
+// 文末に()が付くものをメソッドという特定の処理を行うもの
+// ()で終わらないものをプロパティと言い、定義された変数の特定の値を保持しているもの
