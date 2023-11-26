bf016e39d50d8e9b1406b89ecfedb062d314e56c
hamadanagisa <kaeno.ana@gmail.com>
2023-11-27 02:22:53 +0900

    変数の宣言とデータ型について

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let name = "hamaie";
+name = "yamauti";
+console.log("hello" + name);
+
+// Nnmber, String, Boolean, Undefined, Null, Symbol
+
+// 動的型付け言語
+let name2 = "str";
+name2 = 12;
+name2 = false;
+name2 = undefined;
+
+console.log(typeof name2)
+// どこにログを置くかによって言語は変わる
