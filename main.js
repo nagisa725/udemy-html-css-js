91405c6866ad0e2fbf1033c54eba9af98f5c5a01
hamadanagisa <kaeno.ana@gmail.com>
2023-11-28 18:16:04 +0900

    ループ,for-in,for-ofの実践

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
-// for(...in)
-const arry = [1,2,3,4,5,6];
+const todos = [
+    {
+        id: 1,
+        title: "Go to sukiya",
+        conmpleted:true
+    },
+    {
+        id: 2,
+        title: "Go to susiro-",
+        conmpleted:false
+    },
+    {
+        id: 3,
+        title: "Go to torikizoku",
+        conmpleted:true
+    },
+]
 
-for(let i in arry){
-    console.log(i);
-}
-// forとinを使って配列をループさせた場合iには0,1,2,3,4,5と表示される
-// for(let i = 0; i < arry.length; a++);　よりも記述が短く簡単なループ
-
-const arry2 = [1,2,3,4,5,6];
-for(let a in arry2){
-console.log(arry[a]);
-}
-// arryに格納されているiを一つずつ表示させる
-
-
-const myData = {
-    name:"hamadanagisa",
-    age:23,
-    birthplace:"神奈川県"
-}
-
-for(let Data in myData){
-    console.log(Data);
-}
-
-
-// for(~of...)
-const arry3 = [1,2,3,4,5,6];
-
-for(let v of arry){
-    console.log(v);
+for(let i = 0; i < todos.length; i++) {
+    // console.log(i,todos[i]);
+    let todo = todos[i]
+    if(todo.conmpleted === true){
+        console.log(i,todo.title);
+    }
 }
 
-const myData2 = ["hamada","nagisa",23,"神奈川県"]
+// for-inの場合
+for(let v in todos){
+    let todo = todos[v];
+    // console.log(todos[v])
+    if(todo.conmpleted === true){
+        console.log(v,todo.title)
+    }
+}
 
-for(let data2 of myData2){
-    console.log(data2)
-};
+// for-ofの場合
+for(let todo of todos){
+    // console.log(todo);
+    if(todo.conmpleted===true){
+        console.log(todo.title);
+    }
+}
 
-// for(...in)と for(~of...)の違い
 
-// for(...in)
-// 出力されるのはキーのみ、name,age,birthplaceなど
-// 値である"hamadanagisa"や23は出ない
-// 対してfor(~of...)は配列などの値を順に出力させる
+// タイトルを表示させたい時はconsole.log(i,todos[i],.taitle);とドット記法を使用
+// 完了しているもののみ表示させたい場合に使うのがif文
 
 
