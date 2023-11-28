338ec32d12472a60b38194f84725bf01ddf01dbb
hamadanagisa <kaeno.ana@gmail.com>
2023-11-28 12:11:40 +0900

    ループの記述方法基礎

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-const person ={
-    name:"hamaie ryuuiti"
-};
-// {}(オブジェクトリテラル)で初期化→定義が一般的なオブジェクトの使い方
+const arry = [1,2,3,4,5];
 
-const person2 = {
-    name: ["hamaie","ryuuiti"],
-    // 配列
-    age:  40,
-    gender: "mele",
-    interests:{
-        sports:"baseball",
-        music:"piano"
-    }
-};
-console.log(person2);
+console.log(arry[1]);
+for(let i = 0; i < 5 ;i = i +1){
+    console.log(arry[i]);
+}
 
-console.log(person2.name);
-// person2.name,gender...などドット記法でログ取得も可能
-// console.log(person2.name[1]);で"hamaie"だけ配列取得も可能
-// また変数の中での定義変更は可能なので、person2.age = 41; と記述すれば上書き出来る
-
-
-
-
+const arry2 = [1,2,3,4,5];
+for(let a = 0; a < arry2.length; a++){
+console.log(arry2[a]);
+}
+// 配列の要素数を取得するプロパティlengthを使用する
+// 配列が幾つでも
 
