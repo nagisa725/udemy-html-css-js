fd775ea0477a0e1633155a0f9fb5ee1293f557e3
hamadanagisa <kaeno.ana@gmail.com>
2023-11-28 11:50:13 +0900

    オブジェクトを使ってデータ管理をする方法

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
-const arry = [1,2,3,4,5,6];
-// 配列は0から始まる
-arry[5] = 9;
-// 配列の中の数字[5]を9に変更したい場合
-console.log(arry);
-// []の中に欲しい数字を記述すると抽出され表記される
-// console.log(arry[4]);
-// constで定義したものであるが、同じ変数内での再定義は可能
-// 同じ変数名で別の配列を再定義するのは不可能
+const person ={
+    name:"hamaie ryuuiti"
+};
+// {}(オブジェクトリテラル)で初期化→定義が一般的なオブジェクトの使い方
 
-const arry2 = [1,2,3,"文字列",false];
-// 数字だけではなく文字列やBooleanも可能
+const person2 = {
+    name: ["hamaie","ryuuiti"],
+    // 配列
+    age:  40,
+    gender: "mele",
+    interests:{
+        sports:"baseball",
+        music:"piano"
+    }
+};
+console.log(person2);
 
-arry.push("new item");
-// .push 配列の末尾に()内の記述が表記される
-arry.shift();
-// .shift 先頭の要素を削除する
-// const val = arry.sift();
-// console.log(val);
-// ↑変数に格納すると取り出した要素を表記できる
-arry.unshift();
-// .unshift 配列の先頭に()内の記述が表記される
-arry.pop();
-// .pop　最後に格納されている配列要素を削除できる
-// const val = arry.pop();
-// console.log(val);
-// ↑変数に格納すると取り出した要素を表記できる
-
-
+console.log(person2.name);
+// person2.name,gender...などドット記法でログ取得も可能
+// console.log(person2.name[1]);で"hamaie"だけ配列取得も可能
+// また変数の中での定義変更は可能なので、person2.age = 41; と記述すれば上書き出来る
 
 
 
