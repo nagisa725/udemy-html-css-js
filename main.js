33a58675f652e380907586da66f666a4c0b84bea
hamadanagisa <kaeno.ana@gmail.com>
2023-11-29 16:02:30 +0900

    アロー関数を使用して簡略記述をするの巻

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
+// 基本的な関数の書き方
+function hello(name){
+    console.log("hello"+name)
+};
+hello("山田花子");
+hello("山田花子 2");
+// 引数に名前を渡す事もできる
+function hello(name="山田太郎"){
+    console.log("hallo"+name)
+};
+hello();
+// 変数に関数を代入した場合
+const hello2 =function(name="山田智子"){
+    console.log("hallo"+name)
+};
+hello2();
+// 上記を使いアロー関数の記述方法(記述の簡略化)
+const hello3 =(name="山田友蔵") => console.log("hallo"+name);
+// funcsion省略、二行目から{}使用
+hello3();
+// 引数に何も渡さない場合()も省略可能
+const hello4 = name => console.log("hallo"+name);
+hello4("原田泰造");
+// 関数の引数が２つ以上になる場合は()が必要
+const hello5 = (name,age) => console.log("hallo"+name+age);
+hello5("出川哲朗",50);
 
-if(false){
-    console.log("this is true");
-// if文 条件に応じて制御を変えたい場合に利用できる
-} else if(true){
-    console.log("this is in else if block");
-// else if 複数の条件に当てはまらない場合に応じた処理を指定できる
-} else {
-    console.log("this is false");
-// else 条件に当てはまらない場合の処理
-}
+// for文の際一緒に使うと実用的なのだ
+const arry = [1,2,3,4,5,6];
 
-// if(false){
-//     console.log("パンはありますか");
-// } else if (false){
-//     console,log("ケーキはありますか");
-// } else {
-//     console.log("暴れる")
-// }
+arry.forEach (value => console.log(value));
 
-const num = 10 ;
-const boo1 = Boolean(num);
-console.log(boo1);
-
-if(!num) {
-    console.log("this is true");
-} else{
-    console.log("this is false");
-}
