8551c87a7010594682c0a964eb9333842ad0966d
hamadanagisa <kaeno.ana@gmail.com>
2023-12-05 17:19:18 +0900

    reduce関数を作ってみたい

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
+// const str = "animation";
+// const strArry = str.split("");
 
-/*forEach 配列データの値1つずつに対してコールバック関数に記述した処理を実行
-配列やオブジェクトの要素に対して順番に処理を行いたい場合に非常に便利
-forEachメソッドの中でコールバック関数が行われている、引数(変数名val)であるに配列一つずつが渡っている状態
-第一引数に配列が渡っている、第二、第三引数も定義できる*/ 
-/* i ...配列の添字(index)*/ 
+// function tag(accu,curr){
+//     return `${accu}<${curr}>`;
+// }
 
-const arry =[1,2,3,4,5];
+// const result = strArry.reduce(tag,"");
+// console.log(result);
 
-arry.forEach(function(val,i,ary){
-    console.log(val,i,ary)
-})
-/*値、添字、配列の順で5回ループする*/ 
+/*reduceを使用し同義の記述を書くならこう*/
 
-// アロー関数
-arry.forEach((val,i,ary) => {
-    console.log(val,i,ary)
-})
-// 記述が一つの場合より短略化できバグが生まれにくい
-arry.forEach (v => console.log(v));
+const str = "animation";
+const strArry = str.split("");
 
-for(let i = 0; i < arry.length; i++){
-    const v = arry[i];
-    console.log(v);
+function tag(accu,curr) {
+    console.log(accu,curr);
+    return `${accu}<${curr}>`;
 }
-// for文とforEach
-// forEach 記述が少ない
+
+function reduce(arry,callback,defaultValue){
+    let accu = defaultValue;
+
+    for(let i=0; i < arry.length; i++){
+    let curr = arry[i];
+        accu = callback(accu,curr);
+    }
+    
+    return accu;
+}
+
+const result = strArry.reduce(tag,"");
+console.log(result);
+
