a89bbcedce99a76eeac2ef6e2e4ab9c2bfddfd3c
hamadanagisa <kaeno.ana@gmail.com>
2023-12-15 13:32:09 +0900

    section7-86,okitas

---
+++ okitas./main.js
@@ -0,0 +1,34 @@
+
+
+// let AB= 0;
+// for(let i = 0; i < 100; i++){
+//     AB += i+1;
+// }
+// console.log(AB)
+
+
+const C =1000;
+let okaimono=false;
+for(let i = 0; i < C ; i++){
+    const num = C;
+    if(num<i){
+        okaimono=true;
+    }else if(num>i){
+        okaimono=false;
+    }
+}
+console.log(okaimono)
+
+// if(i>500){
+//  console.log('おもちゃが買えます')
+// }else{
+//  console.log('おもちゃは買えない')
+// }
+let AB =(500);
+let c = (700);
+if(AB>700){
+    console.log('yes');
+}else{
+    console.log('no')
+}
+
---
+++ section7-86/main.js
@@ -0,0 +1,33 @@
+document.addEventListener ('DOMContentLoaded', function () {
+    
+    const els = document.querySelectorAll('.animate-title');
+});
+    //'.animate-title'全てを取得したいのでAllを使用しelsに格納
+const cb = function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            const ta = new TextAnimation('.animate-title');
+            ta.animate();
+            observer.unobserve(entry.target);
+        } else {
+            // entry.target.classList.remove('inview');
+        }
+    });
+    // alert('intersecting');
+}
+const options = {
+    root: null,
+    /*root ある要素が他の要素またはビューポートと交差したときtrueと判定するか決められる機能 
+        ただ親要素,先祖要素のみの指定に限られるためそれ以外はnullが良い*/
+    rootMargin: "-300px 0px",
+    /*rootMargin  root要素に対して作用させるオプションcssのmaginと同様、交差の有効範囲を変化させられる*/ 
+    threshold: [0, 0.5, 1]
+    /*threshold 上記に比べtarget側('.child')に指定するプロパティ,target領域の何％以上が重なったら交差している。とみなすかを指定する
+    境界,しきい値という意味　値表記は0〜1の範囲。1が100%、0が0%を意味する*/ 
+};
+const io = new IntersectionObserver(cb, options);
+    /*new演算子 複数のプログラムが同じオブジェクトに対し干渉しバグが起きないためにオブジェクト本体からコピーし処理を実行するためのもの*/ 
+    /*IntersectionObserver(交差オブサーバー) 指定した範囲(root)に、
+    監視している対象(observe())の要素が入ってきたら(交差したら)その要素に対して処理を実行できる*/ 
+els.forEach(el => io.observe(els));
+//elsに格納されている値がforEachで一つずつ処理される 複数処理のため関数使用
